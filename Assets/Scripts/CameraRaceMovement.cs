45da6736b540b7d73ea23d4f1b6db5f0290370d1
Capuano, German <capuano@gatech.edu>
2016-10-30 11:28:30 -0400

    improved racemode scene

--- Assets/Scripts/CameraRaceMovement.cs
+++ Assets/Scripts/CameraRaceMovement.cs
@@ -3,59 +3,56 @@ using System.Collections;
 
 public class CameraRaceMovement : MonoBehaviour {
 
-	public Transform[] targets;
+    public Transform[] targets;
     public Transform[] goals;
     public float cameraSpeed = 2f;
 
-    public int currentGoal = 0; //public so you can set the starting goal
+    public int currentGoal = 1; //public so you can set the starting goal, min should be 1
 
-	private Vector3 offset;
+    private Vector3 offset;
+    private Vector3 lookPosition;
+    private float initialFOV;
 
-	private Vector3 lookPosition;
+    private float maxz;
 
-	private float initialFOV;
-
-
-	// Use this for initialization
-	void Start () {
-		offset = transform.position;
+    // Use this for initialization
+    void Start() {
+        offset = transform.position;
 
         initialFOV = GetComponent<Camera>().fieldOfView;
-		lookPosition = Vector3.zero;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 leadPosition = targets[GetLeader()].position;
+        lookPosition = Vector3.zero;
+        maxz = goals[0].transform.position.z;
 
-        GetComponent<Camera>().fieldOfView = LevelManager.instance.BeatValue(0f)/2f + initialFOV;
-		transform.GetChild(0).GetComponent<Camera>().fieldOfView = LevelManager.instance.BeatValue(0f)/2f + initialFOV;
-
-		transform.localPosition = Vector3.MoveTowards(transform.localPosition,leadPosition + offset, Time.deltaTime*Vector3.Distance(transform.localPosition,leadPosition + offset)*cameraSpeed);
-		//lookPosition = Vector3.MoveTowards(lookPosition,leadPosition,Time.deltaTime*Vector3.Distance(lookPosition,leadPosition));
-		//transform.LookAt(lookPosition);
-
-	}
-
-    int GetLeader() {
-        int currentLeader = 0;
-        int previousGoal = currentGoal - 1;
-        if (previousGoal < 0) previousGoal = goals.Length - 1;
-        Vector3 raceDirection = (goals[currentGoal].position - goals[previousGoal].position).normalized;
-
-        //checking which of the players reached the farthest position
-        float maxTravelledDistance = -100000; //towards current goal
-        for(int i = 0; i<targets.Length; i++) { 
-            float travelled = Vector3.Dot(targets[i].position - goals[previousGoal].position, raceDirection);
-            if (travelled > maxTravelledDistance) {
-                currentLeader = i;
-                maxTravelledDistance = travelled;
-            }
-        }
-        currentLeader = 0;
-        //changing the goal to next one if necessary
-        if (maxTravelledDistance > (goals[currentGoal].position - goals[previousGoal].position).magnitude)
-            currentGoal = currentGoal == goals.Length - 1 ? 0 : currentGoal + 1;
-        return currentLeader;
     }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 leadPosition = GetCurrentViewTarget();
+
+        GetComponent<Camera>().fieldOfView = LevelManager.instance.BeatValue(0f) / 2f + initialFOV;
+        transform.GetChild(0).GetComponent<Camera>().fieldOfView = LevelManager.instance.BeatValue(0f) / 2f + initialFOV;
+
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, leadPosition + offset, Time.deltaTime * Vector3.Distance(transform.localPosition, leadPosition + offset) * cameraSpeed);
+        //lookPosition = Vector3.MoveTowards(lookPosition, leadPosition, Time.deltaTime * Vector3.Distance(lookPosition, leadPosition));
+        //transform.LookAt(lookPosition);
+
+    }
+
+
+    Vector3 GetCurrentViewTarget() {
+        //update the farthest distance travelled
+        foreach (Transform target in targets)
+            if (target.position.z > maxz)
+                maxz = target.position.z;
+        //check if the updated z position is farther than the current goal and update it if necessary
+        if (maxz > goals[currentGoal].position.z)
+            currentGoal++;
+        //interpolate the target position for the camera between goals
+        float normalizedDistance = (maxz - goals[currentGoal - 1].position.z) / (goals[currentGoal].position.z - goals[currentGoal - 1].position.z);
+        Vector3 direction = (goals[currentGoal].position - goals[currentGoal - 1].position); //not normalized
+        return (goals[currentGoal - 1].position + direction * normalizedDistance);
+    }
+
 }
+
+    
